ead20fd97dd9ff45ed3db03ee00346d5f4ba7c8c
George Barh <georgebarhabas@gmail.com>
2024-08-03 15:13:29 +0000

    Set the structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incremenrScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtrackQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
